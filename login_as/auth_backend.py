76bf05187188a8284969a986b3cf5ef827856822
Jon Bolt <epic.bagel@gmail.com>
2013-06-24 10:25:23 +0100

    Django 1.5 user model

--- login_as/auth_backend.py
+++ login_as/auth_backend.py
@@ -1,17 +1,17 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class LoginAsBackend(object):
     def authenticate(self, from_user, to_username):
         if not from_user.is_superuser:
             return None
         try:
-            user = User.objects.get(username=to_username)
-        except User.DoesNotExist:
+            user = get_user_model().objects.get(username=to_username)
+        except get_user_model().DoesNotExist:
             return None
         return user
 
     def get_user(self, user_id):
         try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
+            return get_user_model().objects.get(pk=user_id)
+        except get_user_model().DoesNotExist:
             return None
