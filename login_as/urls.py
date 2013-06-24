3b0de0f92363d00895f1d3174b7de49cc4013e26
Jon Bolt <epic.bagel@gmail.com>
2013-06-24 11:07:06 +0100

    Update URLs

--- login_as/urls.py
+++ login_as/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('login_as.views',
+    url(r'^user/(?P<user_id>\d+)', 'login', {}, name = 'login-as-login'),
     url('^$', 'chooser', name='login-as-chooser'),
-    url('^(.+?)/$', 'login', name='login-as-login'),
+    #url('^(.+?)/$', 'login', name='login-as-login'),
 )
